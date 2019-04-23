d63412e3a77ca1cb94a11b6f7c6930ee7b4b32dc
Matt Hayes <matt@mysterycommand.com>
2019-04-23 13:00:59 -0400

    refactor(splash): just poking around trying to get this set up

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,27 +1,58 @@
-import React from 'react';
-import logo from '../../logo.svg';
+import React, { Component, MouseEvent } from 'react';
+import logo from '../../logo.png';
+
+import { AppConfig, UserSession } from 'blockstack';
 
 import './style.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppState = {
+  appConfig: AppConfig;
+  userSession: UserSession;
 };
+
+class App extends Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+
+    const appConfig = new AppConfig();
+    const userSession = new UserSession({ appConfig });
+
+    this.state = {
+      appConfig,
+      userSession,
+    };
+  }
+
+  signIn = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    this.state.userSession.redirectToSignIn();
+  };
+
+  signOut = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    this.state.userSession.signUserOut();
+  };
+
+  render() {
+    const { userSession } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {userSession.isUserSignedIn() ? (
+            <button onClick={this.signOut} className="App-button">
+              Sign out
+            </button>
+          ) : (
+            <button onClick={this.signIn} className="App-button">
+              Sign in
+            </button>
+          )}
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
