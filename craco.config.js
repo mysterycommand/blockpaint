93373278c91f3b2f0f8dc14d769ad4c9d8bb9705
Matt Hayes <matt@mysterycommand.com>
2019-04-24 11:13:32 -0400

    test(craco.config.js): now that we're using craco, we can override the testMatch and my tests can ju

--- craco.config.js
+++ craco.config.js
@@ -2,4 +2,10 @@ const CracoBlockstack = require('craco-blockstack');
 
 module.exports = {
   plugins: [{ plugin: CracoBlockstack }],
+  jest: {
+    configure: config => ({
+      ...config,
+      testMatch: [...config.testMatch, '<rootDir>/src/**/tests.{j,t}s?(x)'],
+    }),
+  },
 };
