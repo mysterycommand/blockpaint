6d22550b075d766c23929bb27243b3870184828e
Matt Hayes <matt@mysterycommand.com>
2019-04-25 11:51:59 -0400

    refactor(app/index): shuffling some stuff around for clarity, getting ready to move into splace/work

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -26,12 +26,13 @@ class App extends Component<{}, AppState> {
 
   signIn = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
+    const { userSession } = this.state;
 
     /**
      * @see: https://github.com/zone117x/blockstack-monero/blob/master/src/main.ts#L40-L46
      */
     const { origin, pathname } = window.location;
-    this.state.userSession.redirectToSignIn(
+    userSession.redirectToSignIn(
       `${origin}${pathname}`,
       `${origin}${pathname}manifest.json`,
     );
@@ -39,41 +40,41 @@ class App extends Component<{}, AppState> {
 
   signOut = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    this.state.userSession.signUserOut();
+    const { userSession } = this.state;
 
+    userSession.signUserOut();
     const { origin, pathname } = window.location;
     window.location.assign(`${origin}${pathname}`);
   };
 
-  render() {
+  UNSAFE_componentWillMount() {
     const { userSession } = this.state;
 
-    const isUserSignedIn = userSession.isUserSignedIn();
-    const isSignInPending = userSession.isSignInPending();
-
-    let content = (
-      <button onClick={this.signIn} className="App-button">
-        Sign in
-      </button>
-    );
-
-    if (isUserSignedIn) {
-      const { profile } = userSession.loadUserData();
-      const person = new Person(profile);
-
-      content = <Canvas person={person} signOut={this.signOut} />;
-    } else if (isSignInPending) {
+    if (userSession.isSignInPending()) {
       userSession.handlePendingSignIn().then((/* userData */) => {
         const { origin, pathname } = window.location;
         window.location.assign(`${origin}${pathname}`);
       });
     }
+  }
+
+  render() {
+    const { userSession } = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {content}
+          {userSession.isUserSignedIn() ? (
+            <Canvas
+              person={new Person(userSession.loadUserData().profile)}
+              signOut={this.signOut}
+            />
+          ) : (
+            <button onClick={this.signIn} className="App-button">
+              Sign in
+            </button>
+          )}
         </header>
       </div>
     );
