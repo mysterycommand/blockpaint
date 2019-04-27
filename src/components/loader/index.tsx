bfeac21a1182cd09e2e2e913d6d00f18bc6b9b7d
Matt Hayes <matt@mysterycommand.com>
2019-04-27 09:38:59 -0400

    feat(loader): add a loader/spinner from https://loading.io/css/

---
+++ src/components/loader/index.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import './style.css';
+
+type LoaderProps = {};
+
+const Loader: FC<LoaderProps> = () => (
+  <div className="loader">
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loader;
